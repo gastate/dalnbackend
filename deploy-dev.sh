a021f410d868f254a9d5475fc7e2a8e8952ede26
Korey Kolberg <kolbergconsulting@gmail.com>
2019-10-21 17:26:28 -0400

    Update deploy-dev.sh

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,4 +1,4 @@
-mvn deploy -f ./pom-dev.xml -X
+mvn deploy -f ./pom-dev.xml
 
 sleep 10
 aws apigateway put-rest-api --rest-api-id h10w14u1wd --mode overwrite --body "file://./org.lambadaframework.thedaln-development-development-swagger-apigateway.yaml"
